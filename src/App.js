ff9af53b8a74853ae64191600da862e377a8595a
Shubham Raj <shubhamraj7393@gmail.com>
2020-09-24 11:33:54 +0530

    added lazy load

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Components/Header";
-import Home from "./Pages/Home";
-import PostDetail from "./Pages/PostDetail";
+
+const Home = React.lazy(() => import("./Pages/Home"));
+const PostDetail = React.lazy(() => import("./Pages/PostDetail"));
 
 function App() {
   return (
@@ -12,14 +13,16 @@ function App() {
         <div className="container p-4">
           <div className="row">
             <div className="col-lg-8 col-md-10 mx-auto">
-              <Switch>
-                <Route exact path="/">
-                  <Home />
-                </Route>
-                <Route path="/post-detail">
-                  <PostDetail />
-                </Route>
-              </Switch>
+              <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                  <Route exact path="/">
+                    <Home />
+                  </Route>
+                  <Route path="/post-detail">
+                    <PostDetail />
+                  </Route>
+                </Switch>
+              </Suspense>
             </div>
           </div>
         </div>
